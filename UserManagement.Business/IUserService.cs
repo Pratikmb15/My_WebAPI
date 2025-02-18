f2e919d0fdf803de6c6a678cc3336be93574a150
Pratikmb15 <pratikbagul1508@gmail.com>
2025-02-18 12:39:52 +0530

    Add files via upload

---
+++ UserManagement.Business/IUserService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UserManagement.Data.Models;
+
+namespace UserManagement.Business
+{
+    public interface IUserService
+    {
+        IEnumerable<User> GetAllUsers();
+        User GetUser(int id);
+        void CreateUser(User user);
+        void UpdateUser(User user);
+        void DeleteUser(int id);
+    }
+}
---
+++ UserManagement.Business/UserService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UserManagement.Data.Models;
+using UserManagement.Data;
+
+namespace UserManagement.Business
+{
+    public class UserService : IUserService
+    {
+        private readonly IUserRepository _userRepository;
+
+        public UserService(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public IEnumerable<User> GetAllUsers() => _userRepository.GetUsers();
+
+        public User GetUser(int id) => _userRepository.GetUserById(id);
+
+        public void CreateUser(User user) => _userRepository.AddUser(user);
+
+        public void UpdateUser(User user) => _userRepository.UpdateUser(user);
+
+        public void DeleteUser(int id) => _userRepository.DeleteUser(id);
+    }
+}
